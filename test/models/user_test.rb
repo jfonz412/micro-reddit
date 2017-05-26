7bfca2c5fafd0d88a71f7933a0022051eca3236a
jfonz412 <jfonzone@outlook.com>
2017-05-26 15:36:19 -0400

    non-blank tests (fail)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+    @user = User.new(name: "Mr. Foo")
+	end
+
+	test "user should be valid" do
+		assert(@user.valid?)
+	end
+
+	test "user.name should be present (not blank)" do
+		@user.name = "		"
+		assert_not(@user.valid?)
+	end
+
 end
