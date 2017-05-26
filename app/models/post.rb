40d6061b38fd4a2c3aeb2b978613c0008173e880
jfonz412 <jfonzone@outlook.com>
2017-05-26 17:13:33 -0400

    post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 3, maximum: 255 }
+	# URL Regex will not accept "shortened" links like foobar.com, must be www.foobar.com
+	VALID_URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+	validates :link, presence: true, length: { maximum: 255 },
+						format: { with: VALID_URL_REGEX }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@post = Post.new(title: "Foo Bar", link: "https://foo_bar.com", user_id: 1)
+	end
+
+	test "post is valid" do
+		assert(@post.valid?)
+	end
+
+	test "post title cannot be blank" do
+		@post.title = "			"
+		assert_not(@post.valid?)
+	end
+
+	test "post link cannot be blank" do
+		@post.link = "			"
+		assert_not(@post.valid?)
+	end
+
+	test "post title cannot be less than 3 chars" do
+		@post.title = "xy"
+		assert_not(@post.valid?)
+	end
+
+	test "post title cannot be more than 255 chars" do
+		@post.title = "x" * 256
+		assert_not(@post.valid?)
+	end
+
+	test "link cannot be more than 255 chars" do
+		@post.link = "x" * 256
+		assert_not(@post.valid?)
+	end
+
+	test "should accept valid links" do
+		valid_urls = %w[www.foobar.com https://foobar.com www.foo-bar.com https://www.foobar.gov]
+		valid_urls.each do |valid_url|
+			@post.link = valid_url
+			assert(@post.valid?, "#{valid_url.inspect} should be valid")
+		end
+	end
+
+	test "link must be a valid URL" do
+		invalid_urls = %w[foobar f.o.o.b.a.r.com https//foobar.com wwwfoobarcom]
+		invalid_urls.each do |invalid_url|
+			@post.link = invalid_url
+			assert_not(@post.valid?, "#{invalid_url.inspect} should be invalid")
+		end
+	end
 end
