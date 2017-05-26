c76f8b7a74a04226035e6efaa9b80fae09afbac2
jfonz412 <jfonzone@outlook.com>
2017-05-26 15:49:15 -0400

    enforce name uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,4 +15,10 @@ class UserTest < ActiveSupport::TestCase
 		assert_not(@user.valid?)
 	end
 
+	test "user's name should be unique" do
+		duplicate_user = @user.dup
+		@user.save
+		assert_not(duplicate_user.valid?)
+	end
+
 end
