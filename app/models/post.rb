d0189b469ab1c6171f5cc4eaca5c9848d6f323d3
jfonz412 <jfonzone@outlook.com>
2017-05-26 18:54:19 -0400

    add comments to new association lines

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	belongs_to :user
+	belongs_to :user # generated via migration, but this line was manually added
 	validates :title, presence: true, length: { minimum: 3, maximum: 255 }
 	# URL Regex will not accept "shortened" links like foobar.com, must be www.foobar.com
 	VALID_URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	has_many :posts
+	has_many :posts #manually added, no migration needed
 	validates :name, presence: true, uniqueness: true,
 						length: { maximum: 25, minimum: 3 }
 end
