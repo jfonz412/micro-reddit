3b8b2430c24047920669accf94570092815793d0
jfonz412 <jfonzone@outlook.com>
2017-05-27 13:41:43 -0400

    comment-post realtionship

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+	
 	has_many :comments
 	belongs_to :user # generated via migration, but this line was manually added
+
 	validates :title, presence: true, length: { minimum: 3, maximum: 255 }
 	# URL Regex will not accept "shortened" links like foobar.com, must be www.foobar.com
 	VALID_URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527172151) do
+ActiveRecord::Schema.define(version: 20170527173804) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
