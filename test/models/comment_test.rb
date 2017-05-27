94f913b0bb23af9c916b43386c80971643f4358e
jfonz412 <jfonzone@outlook.com>
2017-05-27 13:12:30 -0400

    comment validations

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		User.create(name: "Mr. Foo")
+		User.first.posts.build(title: "Foo Bar", link: "www.foobar.com")
+		@comment = Comment.new(text:"This is the first ever micro-reddit comment.")
+	end
+
+	test "should be valid" do
+		assert(@comment.valid?)
+	end
+
+	test "comment cannot be blank" do
+		@comment.text = "			"
+		assert_not(@comment.valid?)
+	end
+
+	test "comments cannot be longer than 10,000 chars" do
+		@comment.text = "x" * 10001
+		assert_not(@comment.valid?)
+	end
 end
